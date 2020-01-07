b67a17022adfa1e4b9f56505e0723a9ddb3da6f3
Lion Erez <lionerez1@gmail.com>
2020-01-08 01:15:55 +0200

    Initial Commit

---
+++ CarouselAnimationFramework/CarouselAnimationFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarouselAnimationFramework.h
+//  CarouselAnimationFramework
+//
+//  Created by Lion Erez on 08/01/2020.
+//  Copyright © 2020 Lion Erez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarouselAnimationFramework.
+FOUNDATION_EXPORT double CarouselAnimationFrameworkVersionNumber;
+
+//! Project version string for CarouselAnimationFramework.
+FOUNDATION_EXPORT const unsigned char CarouselAnimationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselAnimationFramework/PublicHeader.h>
+
+
